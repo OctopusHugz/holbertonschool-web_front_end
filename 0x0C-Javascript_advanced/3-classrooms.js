9db2baa39276adb6f413729f29f36e6546e441a5
OctopusHugz <colsonscott53@gmail.com>
2021-02-15 20:31:31 +0000

    Added 3-classrooms.js

---
+++ 0x0C-Javascript_advanced/3-classrooms.js
@@ -0,0 +1,17 @@
+#!/usr/bin/node
+
+function createClassRoom(numberOfStudents) {
+  function studentSeat(seat) {
+    return function () {
+      return seat;
+    };
+  }
+  let students = [];
+  for (let i = 0; i < numberOfStudents; i++) {
+    let student = studentSeat(i + 1);
+    students.push(student);
+  }
+  return students;
+}
+
+let classRoom = createClassRoom(10);
