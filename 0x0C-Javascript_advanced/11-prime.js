571eeb9bc05cd1819a8ead675ac76cafe5111913
OctopusHugz <colsonscott53@gmail.com>
2021-02-16 19:31:24 +0000

    Added outline for 11-prime.js

---
+++ 0x0C-Javascript_advanced/11-prime.js
@@ -0,0 +1,32 @@
+function countPrimeNumbers() {
+  // return # of prime numbers from 2 - 100
+  let primeCount = 0;
+  let start = 2;
+  let end = 101;
+  let isPrime;
+  for (let i = start; i < end; i++) {
+    for (let j = 2; j < i; j++) {
+      if (i % j === 0) {
+        isPrime = false;
+        break;
+      } else isPrime = true;
+    }
+    if (isPrime || i === 2) {
+      primeCount++;
+    }
+    isPrime = null;
+  }
+  return primeCount;
+}
+
+let t1 = performance.now();
+for (let i = 0; i < 100; i++) {
+  countPrimeNumbers();
+}
+let t2 = performance.now();
+let time_elapsed = t2 - t1;
+console.log(
+  "Execution time of calculating prime numbers 100 times was " +
+    time_elapsed +
+    " milliseconds."
+);
