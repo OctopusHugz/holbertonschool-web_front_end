c8122fb5e2464c48d4e7a679c1b4854d0781c29f
OctopusHugz <colsonscott53@gmail.com>
2021-02-19 17:25:51 +0000

    Updated solution using only jQuery

--- 0x0D-JQuery_advanced/scripts/1-index.js
+++ 0x0D-JQuery_advanced/scripts/1-index.js
@@ -1,9 +1,10 @@
 $(document).ready(function () {
   function createTextElement() {
-    let newP = document.createElement("p");
-    newP.innerHTML =
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed in diam risus. Nunc sit amet euismod ipsum. Aenean tempus ex sed est volutpat, sed sodales velit tempus. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Proin auctor sollicitudin eleifend. Vivamus porta enim vitae mauris commodo, vitae tempor tellus elementum. Quisque sed pellentesque nulla, at eleifend nisi. Phasellus eget tincidunt ex. Lorem ipsum dolor sit amet, consectetur adipiscing elit.";
-    document.body.append(newP);
+    let newP = $("<p></p>");
+    $(newP).html(
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed in diam risus. Nunc sit amet euismod ipsum. Aenean tempus ex sed est volutpat, sed sodales velit tempus. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Proin auctor sollicitudin eleifend. Vivamus porta enim vitae mauris commodo, vitae tempor tellus elementum. Quisque sed pellentesque nulla, at eleifend nisi. Phasellus eget tincidunt ex. Lorem ipsum dolor sit amet, consectetur adipiscing elit."
+    );
+    $("body").append(newP);
   }
   createTextElement();
 });
