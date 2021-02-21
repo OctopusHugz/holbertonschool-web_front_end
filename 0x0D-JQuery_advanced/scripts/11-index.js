a3a214200e51e24da81bdba9aea38a26a36770e9
OctopusHugz <colsonscott53@gmail.com>
2021-02-21 01:10:58 +0000

    Added 11-index.js. Need to fix POST failure behavior

---
+++ 0x0D-JQuery_advanced/scripts/11-index.js
@@ -0,0 +1,58 @@
+$(() => {
+  function addPostRow(data) {
+    let newP = $("<p></p>");
+    let newSpan = $("<span></span>");
+    $(newSpan).html(
+      `Post created with id ${data.id}, title: ${data.title}, author: ${data.author}`
+    );
+    $(newP).append(newSpan);
+    $("body").append(newP);
+  }
+
+  function listPosts() {
+    $.get("http://localhost:3000/posts", function (response) {
+      response.forEach((post) => {
+        addPostRow(post);
+      });
+    }).fail(() => {
+      alert("Server Error");
+    });
+  }
+
+  function buildForm() {
+    let newForm = $("<form></form>");
+    let newDiv1 = $(
+      "<div><label for='author'>Author</label><input type='text' id='author'></div>"
+    );
+    let newDiv2 = $(
+      "<div><label for='title'>Title</label><textarea id='title'></textarea></div>"
+    );
+    let newSubmit = $("<input type='submit'>");
+    $("body").append($(newForm).append(newDiv1, newDiv2, newSubmit));
+    let textArea = $("textarea[id='title']");
+    $(newSubmit).click(() => {
+      sendForm();
+    });
+    $(textArea).keypress((e) => {
+      if (e.which === 13) {
+        $(newSubmit).click();
+      }
+    });
+  }
+
+  function sendForm() {
+    let authorInput = $("input[id='author']").val();
+    let titleInput = $("textarea[id='title']").val();
+    let data = { author: authorInput, title: titleInput };
+    let url = "http://localhost:3000/posts";
+    $("form").after("About to send the query to the API");
+    $.post(url, data, function (response) {
+      addPostRow(response);
+    }).fail(() => {
+      alert("Error sending the POST query");
+    });
+  }
+
+  listPosts();
+  buildForm();
+});
