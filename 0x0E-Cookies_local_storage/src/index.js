24bcdbd8b80f792c8f7744f9287b647be0c8c645
OctopusHugz <colsonscott53@gmail.com>
2021-02-23 17:59:29 +0000

    Added 0-index.html and index.js

---
+++ 0x0E-Cookies_local_storage/src/index.js
@@ -0,0 +1,24 @@
+document.addEventListener("DOMContentLoaded", function () {
+  function setCookies() {
+    let firstNameInput = document.getElementById("firstname").value;
+    let emailInput = document.getElementById("email").value;
+    document.cookie = `firstname=${firstNameInput}`;
+    document.cookie = `email=${emailInput}`;
+  }
+
+  function showCookies() {
+    let newP = document.createElement("p");
+    let allCookies = document.cookie;
+    newP.innerHTML = `Cookies: ${allCookies}`;
+    document.body.append(newP);
+    // let allCookies = document.cookie.split(";");
+    // allCookies.forEach((cookie) => {
+    //   cookie = cookie.trim();
+    //   let cookieKey = cookie.split("=")[0];
+    //   let cookieValue = cookie.split("=")[1];
+    //   console.log(cookieValue);
+    // });
+  }
+  document.getElementById("login").addEventListener("click", setCookies);
+  document.getElementById("show").addEventListener("click", showCookies);
+});
