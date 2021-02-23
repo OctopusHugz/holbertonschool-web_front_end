64a47951717c8ec3802c422793e93511d312f812
OctopusHugz <colsonscott53@gmail.com>
2021-02-23 18:49:15 +0000

    Revert index.js to original status for task 0pu

--- 0x0E-Cookies_local_storage/src/index.js
+++ 0x0E-Cookies_local_storage/src/index.js
@@ -2,32 +2,16 @@ document.addEventListener("DOMContentLoaded", () => {
   function setCookies() {
     let firstNameInput = document.getElementById("firstname").value;
     let emailInput = document.getElementById("email").value;
-    document.cookie = `firstname=${firstNameInput};max-age=864000`;
-    document.cookie = `email=${emailInput};max-age=864000`;
+    document.cookie = `firstname=${firstNameInput}`;
+    document.cookie = `email=${emailInput}`;
   }
 
   function showCookies() {
     let newP = document.createElement("p");
-    let emailCookie = getCookie("email");
-    let firstnameCookie = getCookie("firstname");
-    console.log(emailCookie);
-    console.log(firstnameCookie);
-    newP.innerHTML = `Email: ${emailCookie} - Firstname: ${firstnameCookie}`;
+    let allCookies = document.cookie;
+    newP.innerHTML = `Cookies: ${allCookies}`;
     document.body.append(newP);
   }
-
-  function getCookie(name) {
-    try {
-      let cookieValue = document.cookie
-        .split("; ")
-        .find((row) => row.startsWith(name + "="))
-        .split("=")[1];
-      return cookieValue;
-    } catch (error) {
-      return "";
-    }
-  }
-
   document.getElementById("login").addEventListener("click", setCookies);
   document.getElementById("show").addEventListener("click", showCookies);
 });
