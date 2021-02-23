9c212eefa110a1812b00816a702ba9ff5c354826
OctopusHugz <colsonscott53@gmail.com>
2021-02-23 18:08:34 +0000

    Added 1-index.html and updated index.js to set 10 day max-age

--- 0x0E-Cookies_local_storage/src/index.js
+++ 0x0E-Cookies_local_storage/src/index.js
@@ -2,8 +2,8 @@ document.addEventListener("DOMContentLoaded", () => {
   function setCookies() {
     let firstNameInput = document.getElementById("firstname").value;
     let emailInput = document.getElementById("email").value;
-    document.cookie = `firstname=${firstNameInput}`;
-    document.cookie = `email=${emailInput}`;
+    document.cookie = `firstname=${firstNameInput};max-age=864000`;
+    document.cookie = `email=${emailInput};max-age=864000`;
   }
 
   function showCookies() {
