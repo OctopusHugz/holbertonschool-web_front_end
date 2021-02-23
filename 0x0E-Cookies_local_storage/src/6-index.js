f982aae1668ada97b68139c303a8b36884b4d1ee
OctopusHugz <colsonscott53@gmail.com>
2021-02-23 20:21:54 +0000

    Added 6-index.js

---
+++ 0x0E-Cookies_local_storage/src/6-index.js
@@ -0,0 +1,39 @@
+document.addEventListener("DOMContentLoaded", () => {
+  let availableItems = ["Shampoo", "Soap", "Sponge", "Water"];
+  if (typeof Storage !== "undefined") {
+    createStore();
+    displayCart();
+  } else {
+    alert(
+      "Sorry, your browser does not support Web storage. Try again with a better one"
+    );
+  }
+
+  function addItemToCart(item) {
+    sessionStorage.setItem(item, "true");
+  }
+
+  function createStore() {
+    let newUL = document.createElement("ul");
+    availableItems.forEach((item) => {
+      let newLI = document.createElement("li");
+      newLI.innerHTML = item;
+      newLI.addEventListener("click", () => {
+        addItemToCart(item);
+      });
+      newUL.append(newLI);
+    });
+    document.body.append(newUL);
+  }
+
+  function displayCart() {
+    console.log(sessionStorage);
+    if (sessionStorage.length > 1) {
+      let newP = document.createElement("p");
+      newP.innerHTML = `You previously had ${
+        sessionStorage.length - 1
+      } items in your cart`;
+      document.body.append(newP);
+    }
+  }
+});
